6d967e3ab9ac097f5e51f25fdced20f1962407a1
Peter <iw4ffl3z@gmail.com>
2021-02-04 14:46:53 +0100

    Added solutions to exercise 1 and 2 in Intro_Exercises

---
+++ Intro_Exercises/Exercise_1/1.js
@@ -0,0 +1,4 @@
+const firstName = "Anders";
+const lastName = "Latif";
+
+console.log("My first name is " + firstName + " and my last name is " + lastName);
---
+++ Intro_Exercises/Exercise_2/2.js
@@ -0,0 +1,3 @@
+const year = "2020";
+const number = 1;
+console.log(parseInt(year) + number);
