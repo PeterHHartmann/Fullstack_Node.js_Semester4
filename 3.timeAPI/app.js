d8636755f61c26505022193c2ffe2200406fc2f9
Peter <iw4ffl3z@gmail.com>
2021-02-19 15:23:31 +0100

    implemented time, day and month routing to API

--- 3.timeAPI/app.js
+++ 3.timeAPI/app.js
@@ -5,14 +5,40 @@ app.get("/", (req, res) => {
     res.send({});
 });
 
-app.get("/me", (req, res) => {
-    const myInfo = {
-        firstname: "Peter",
-        lastname: "Hartmann",
-        age: 25,
-        country: "Denmark"
-    };
-    res.send(myInfo);
+app.get("/time", (req, res) => {
+    const getTime = (date) => {
+        return {
+            hours: date.getHours(),
+            minutes: date.getMinutes(),
+            seconds: date.getSeconds(),
+            milliseconds: date.getMilliseconds(), 
+        }
+    }
+    res.send(getTime(new Date()));
+});
+
+app.get("/day", (req, res) => {
+    const getDay = (date) => {
+        const day = date.getDay();
+        const daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        return {
+            day: day,
+            dayOfWeek: daysOfWeek[day]
+        }
+    }
+    res.send(getDay(new Date()));
+});
+
+app.get("/month", (req, res) => {
+    const getMonth = (date) => {
+        const month = date.getMonth();
+        const monthsOfYear = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+        return {
+            month: month, 
+            monthOfYear: monthsOfYear[month]
+        }
+    }
+    res.send(getMonth(new Date()));
 });
 
 app.listen(8080);
