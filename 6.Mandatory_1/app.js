b7f010920c1b592d0660c7363d08b55aad734904
Peter <peter.helle.hartmann@gmail.com>
2021-03-24 23:54:29 +0100

    Mandatory 1 implemented

--- 6.Mandatory_1/app.js
+++ 6.Mandatory_1/app.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/frontpage/frontpage.html");
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/public/about/about.html");
+});
+
+app.get("/datatypes", (req, res) => {
+    res.sendFile(__dirname + "/public/data_types/data_types.html");
+});
+
+app.get("/nodepackagemanager", (req, res) => {
+    res.sendFile(__dirname + "/public/node_package_manager/node_package_manager.html");
+});
+
+app.get("/nodemon", (req, res) => {
+    res.sendFile(__dirname + "/public/nodemon/nodemon.html");
+});
+
+app.get("/express_guide", (req, res) => {
+    res.sendFile(__dirname + "/public/express_guide/express_guide.html");
+});
 
 app.listen(8080, () => console.log("listening on http://localhost:8080/"));
