02481ea5bc5336ebe05116b6aff5218dde5d1a2f
Peter <peter.helle.hartmann@gmail.com>
2021-03-25 03:03:15 +0100

    serving HTML files added - final commit for hand-in

--- 6.Mandatory_1/app.js
+++ 6.Mandatory_1/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/frontpage/frontpage.html");
