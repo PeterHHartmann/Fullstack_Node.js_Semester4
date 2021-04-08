f6cb65fc864affef3e13ff4a0b37fbd76673ef2c
Peter <peter.helle.hartmann@gmail.com>
2021-04-08 15:53:57 +0200

    inital commit for Nodefolio with and without Bootstrap

---
+++ 10._Nodefolio-with-Bootstrap/public/frontpage/frontpage.js
@@ -0,0 +1,2 @@
+console.log("frontpage.js loaded");
+document.getElementById("year").innerHTML = "Copyright © " + new Date().getFullYear();
---
+++ 9._Nodefolio/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+
+const fs = require('fs');
+const header = fs.readFileSync(__dirname + '/public/header/header.html', "utf-8");
+const footer = fs.readFileSync(__dirname + '/public/footer/footer.html', "utf-8");
+const frontpage = fs.readFileSync(__dirname + '/public/frontpage/frontpage.html', "utf-8");
+
+app.get("/", (req, res) => {
+    // res.sendFile(__dirname + "/public/frontpage/frontpage.html");
+    res.send(header + frontpage + footer);
+});
+
+app.get("/projects", (req, res) => {
+    res.sendFile(__dirname + "/public/projects/projects.html");
+});
+
+const server = app.listen(PORT, (error) => {
+    if(error){
+        console.log(error);
+    } else {
+        console.log("Server is running on port " + server.address().port);
+    }
+});
